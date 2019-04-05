8992e1192083a43789ec834c1d0ef060dc6f98dd
miinnii <48317907+miinnii@users.noreply.github.com>
2019-04-05 18:01:13 +0300

    Rewriting of some console text, and spesification to make it easier to read and understand, and some spelling corrections

--- src/main/java/CustomerUI.java
+++ src/main/java/CustomerUI.java
@@ -18,11 +18,11 @@ public class CustomerUI
         boolean exit = false;
         while (!exit)
         {
-            System.out.println("Type 1 to create ticket:");
-            System.out.println("Type 2 to view events and participants:");
-            System.out.println("Type 3 to refund ticket:");
-            System.out.println("Type 4 to check if ticket is valid:");
-            System.out.println("Type 5 to use ticket:");
+            System.out.println("Type 1 to buy a ticket:");
+            System.out.println("Type 2 to view available events and their participants:");
+            System.out.println("Type 3 to refund a ticket:");
+            System.out.println("Type 4 to check if a ticket is valid:");
+            System.out.println("Type 5 to use a ticket:");
             System.out.println("Type 99 to exit to main menu:");
             int input = Integer.parseInt(scanner.nextLine());
 
@@ -61,10 +61,10 @@ public class CustomerUI
             System.out.println("-" + event.getName());
         }
 
-        System.out.println("Select event by writing the event name:");
+        System.out.println("Select an event by writing the events name:");
         String eventToPurchaseTicket = scanner.nextLine();
 
-        System.out.println("CustomerName: ");
+        System.out.println("Input your name: ");
         String customerName = scanner.nextLine();
 
         for (Event event : availableEvents)
@@ -77,7 +77,7 @@ public class CustomerUI
                 }
                 else
                 {
-                    System.out.println("Something went wrong when crating the ticket :( ");
+                    System.out.println("Something went wrong when creating the ticket :(\nTry again later");
                 }
             }
         }
@@ -91,7 +91,7 @@ public class CustomerUI
             System.out.println(event.getName());
         }
 
-        System.out.println("For waht event do you want to refund your ticket?");
+        System.out.println("For which event do you want to refund your ticket?");
         String eventName = scanner.nextLine();
         System.out.println("What name did you use when buying the ticket?");
         String customerName = scanner.nextLine();
@@ -102,32 +102,34 @@ public class CustomerUI
             {
                 if (ticketService.refundTicket(customerName, event))
                 {
-                    System.out.println("The ticket was refunded");
+                    System.out.println("The ticket was refunded successfully");
                 }
                 else
                 {
-                    System.out.println("The ticket was not valid for a refund");
+                    System.out.println("The ticket you specified was not valid for a refund");
                 }
             }
         }
     }
 
-    private void viewEventsAndParticipants()
-    {
+    private void viewEventsAndParticipants() {
         List<Event> availableEvents = eventService.getEvents();
-        for(Event event : availableEvents)
-        {
-            System.out.println("-" + event.getName());
-            for(Ticket ticket : event.getTickets())
-            {
-                System.out.println("--" + ticket.getOwner());
+        if (availableEvents.isEmpty()) {
+            System.out.println("There are no available events: try again later, or make your own");
+        } else {
+            System.out.println("Number of available events: " + availableEvents.size());
+            for (Event event : availableEvents) {
+                System.out.println("-" + event.getName());
+                for (Ticket ticket : event.getTickets()) {
+                    System.out.println("--" + ticket.getOwner());
+                }
             }
         }
     }
 
     private void checkIfTicketIsValid()
     {
-        System.out.println("For what event do you want to check if your ticket is valid?");
+        System.out.println("For which event do you want to check if your ticket is valid?");
         String eventName = scanner.nextLine();
         System.out.println("What name did you use when buying the ticket?");
         String customerName = scanner.nextLine();
@@ -144,18 +146,21 @@ public class CustomerUI
 
     private void useTicket()
     {
-        System.out.println("What event do you want to use your ticket for?");
+        System.out.println("Which event do you want to use your ticket for?");
         String eventName = scanner.nextLine();
         System.out.println("What name did you use when buying the ticket?");
         String customerName = scanner.nextLine();
 
-        if (eventService.useTicket(eventName, customerName))
+        if(!eventService.checkTicketValid(eventName,customerName)) {
+            System.out.println("Your ticket is invalid.\nGet a valid ticket and try again");
+        }
+        else if (eventService.useTicket(eventName, customerName))
         {
             System.out.println("The ticket was used successfully");
         }
         else
         {
-            System.out.println("Something went wrong when using the ticket :(");
+            System.out.println("Something went wrong when using the ticket :( \nTry again later");
         }
     }
 }
--- src/main/java/OrganizerUI.java
+++ src/main/java/OrganizerUI.java
@@ -19,9 +19,9 @@ public class OrganizerUI
         boolean exit = false;
         while (!exit)
         {
-            System.out.println("Type 1 to create event:");
-            System.out.println("Type 2 to view events and participants:");
-            System.out.println("Type 99 to exit to main menu:");
+            System.out.println("Type 1 to create an event:");
+            System.out.println("Type 2 to view available events and their participants:");
+            System.out.println("Type 99 to go back to the main menu:");
             int input = Integer.parseInt(scanner.nextLine());
 
             switch (input)
@@ -43,40 +43,42 @@ public class OrganizerUI
 
     private void createEvent()
     {
-        System.out.println("Event name: ");
+        System.out.println("Enter a name for your event: ");
         String eventName = scanner.nextLine();
 
-        System.out.println("Event Location: ");
+        System.out.println("Enter the location for your event: ");
         String eventLocation = scanner.nextLine();
 
-        System.out.println("Event Description: ");
+        System.out.println("Enter a description of your event: ");
         String eventDescription = scanner.nextLine();
 
-        System.out.println("Event MaxNumberOfTickets: ");
+        System.out.println("What is your events max number of participants: ");
         int eventMaxNumberOfTickets = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Event TicketPrice: ");
+        System.out.println("Input a price for your events tickets: ");
         int eventTicketPrice = Integer.parseInt(scanner.nextLine());
 
         if (eventService.createEvent(eventName, eventLocation, eventDescription, eventMaxNumberOfTickets, eventTicketPrice))
         {
-            System.out.println("Event created successfully");
+            System.out.println("Your event was created successfully.\nThank you for using our service.");
         }
         else
         {
-            System.out.println("Something went wrong when crating the event :( ");
+            System.out.println("Something went wrong when creating your event :( \nTry again later");
         }
     }
 
-    private void viewEventsAndParticipants()
-    {
+    private void viewEventsAndParticipants() {
         List<Event> availableEvents = eventService.getEvents();
-        for(Event event : availableEvents)
-        {
-            System.out.println("-" + event.getName());
-            for(Ticket ticket : event.getTickets())
-            {
-                System.out.println("--" + ticket.getOwner());
+        if (availableEvents.isEmpty()) {
+            System.out.println("There are no available events: try again later, or make your own");
+        } else {
+            System.out.println("Number of available events: " + availableEvents.size());
+            for (Event event : availableEvents) {
+                System.out.println("-" + event.getName());
+                for (Ticket ticket : event.getTickets()) {
+                    System.out.println("--" + ticket.getOwner());
+                }
             }
         }
     }

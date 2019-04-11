ebf191fe68eb0e756341570184d2f6d78d255cb6
miinnii <48317907+miinnii@users.noreply.github.com>
2019-04-11 13:15:45 +0300

    Little change
    little

--- src/test/java/Mail_Test.java
+++ src/test/java/Mail_Test.java
@@ -1,4 +1,4 @@
-import org.junit.Before;
+
 import org.junit.Test;
 
 import javax.mail.MessagingException;
@@ -7,20 +7,6 @@ import static junit.framework.TestCase.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Mail_Test {
-
-    private EventService eventService;
-    private TicketService ticketService;
-    private PayService payService;
-
-    @Before
-    public void setup()
-    {
-        EventRepository eventRepository = new EventRepository();
-        TicketRepository ticketRepository = new TicketRepository();
-        payService = new PayService();
-        ticketService = new TicketService(ticketRepository, payService);
-        eventService = new EventService(eventRepository);
-    }
 
     @Test
     public void sends_ticket_to_mail(){ //if it doesnt work, it might be firewall, so try turning it off and try again, should work with code
